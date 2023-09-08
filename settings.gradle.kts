5545a54d503c9489b383f25a17f42b9f44e7a435
Cris <cristian.abab@gmail.com>
2023-09-08 17:53:22 -0300

    git config user.email "cristian.abab@gmail.com"   git config user.name "cristian"

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EjemploIPV052023"
+include(":app")
+ 
