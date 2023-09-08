90b1e68d1cc46ee645ce4f1646f250a65bfef92e
Cris <cristian.abab@gmail.com>
2023-09-08 17:42:12 -0300

    git config user.email "cristian.abab@gmail.com"   git config user.name "cristian"

---
+++ app/src/main/java/cl/virginio233/ejemploipv052023/MainActivity5.java
@@ -0,0 +1,14 @@
+package cl.virginio233.ejemploipv052023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+    }
+}
